35a40b51c0d04e17804af5ede56e07f8421106a1
Dong Zeng <dong@freshbytesoftware.com>
2021-04-17 13:25:29 -0500

    add net5 console project to play with c# record

---
+++ Records/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Records
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var classroomCourse = new Course(
+                "Name Immutable",
+                "Author Immutable"
+                );
+        }
+    }
+
+    public record Course(string Name, string Author);
+}
