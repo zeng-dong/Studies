a1a992e39b4156f09daf03574d1ec437c4576b22
Dong Zeng <dong@freshbytesoftware.com>
2021-04-18 21:00:00 -0500

    config and run consumer, together with producer

--- Kafka/Program.cs
+++ Kafka/Program.cs
@@ -1,8 +1,11 @@
 ﻿using Confluent.Kafka;
+using Kafka.Public;
+using Kafka.Public.Loggers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,10 +22,43 @@ namespace Kafka
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((context, collection) =>
                 {
-
+                    collection.AddHostedService<KafkaConsumerHostedService>();
                     collection.AddHostedService<KafkaProducerHostedService>();
                 });
     }
+
+    public class KafkaConsumerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaConsumerHostedService> _logger;
+        private readonly ClusterClient _cluster;
+
+        public KafkaConsumerHostedService(ILogger<KafkaConsumerHostedService> logger)
+        {
+            _logger = logger;
+            _cluster = new ClusterClient(new Configuration
+            {
+                Seeds = "localhost:9092"
+            }, new ConsoleLogger());
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _cluster.ConsumeFromLatest("demo");
+            _cluster.MessageReceived += record =>
+            {
+                _logger.LogInformation($"Received: {Encoding.UTF8.GetString(record.Value as byte[])}");
+            };
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _cluster?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+
 
     public class KafkaProducerHostedService : IHostedService
     {
