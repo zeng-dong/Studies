5258ae83b6ae3442754287f6f7c9020e4d6a47b3
Dong Zeng <dong@freshbytesoftware.com>
2021-04-20 10:49:04 -0500

    start eventsourcing, adding a few events

---
+++ EventSourcing/IEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EventSourcing
+{
+    public interface IEvent
+    {
+    }
+
+    public record ProductShipped(string Sku, int Quantity, DateTime DateTime) : IEvent;
+
+    public record ProductReceived(string Sku, int Quantity, DateTime DateTime) : IEvent;
+
+    public record InventoryAdjusted(string Sku, int Quantity, DateTime DateTime) : IEvent;
+
+
+}
---
+++ EventSourcing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventSourcing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
