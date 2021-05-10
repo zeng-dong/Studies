ae0f0c951b3e4d7155a7f06897bd1e033f91e428
Dong Zeng <dong@freshbytesoftware.com>
2021-05-09 21:39:54 -0500

    added Helper module

--- IceCreamTruckFsharp/Program.fs
+++ IceCreamTruckFsharp/Program.fs
@@ -19,11 +19,20 @@ module Domain =
         | Flavour_went_out_of_stock of Flavour
         | Flavour_was_not_in_stock of Flavour
 
+module Helper =
+    let printUl list =
+        list 
+        |> List.iteri(fun i item -> printfn " %i: %A" (i+1) item)
 
+    let printEvents events =
+        events
+        |> List.length
+        |> printfn "History (Length: %i)"
 
 
 open Infrastructure
 open Domain
+open Helper
 
 [<EntryPoint>]
 let main _ =
@@ -31,4 +40,11 @@ let main _ =
     let eventStore : EventStore<Event> = EventStore.initialize()
 
     eventStore.Append [Flavour_restocked (Vanilla,3)]
+    eventStore.Append [Flavour_sold Vanilla]
+    eventStore.Append [Flavour_sold Vanilla]
+    eventStore.Append [Flavour_sold Vanilla; Flavour_went_out_of_stock Vanilla]
+
+    eventStore.Get()
+    |> printEvents
+
 
