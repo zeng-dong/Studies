52cb00ecb704030874bf16204b6b36136cf17715
Dong Zeng <dong@freshbytesoftware.com>
2021-05-12 14:31:27 -0500

    EsAndCqrs project, added Account aggregate root

---
+++ EsAndCqrs/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EsAndCqrs
+{
+    public class Account
+    {
+        private Guid Id { get; set; }
+        private decimal CurrentAmount { get; set; }
+
+        public ICollection<Event> UncommitedEvents { get; } = new List<Event>();
+
+        public void Open(string owner, string iban)
+        {
+            UncommitedEvents.Add(new AccountOpened(Id, owner, iban));
+        }
+
+        public void TransferMoney(decimal amount, string iban)
+        {
+            if (CurrentAmount < amount)
+                throw new InvalidOperationException("Not enough money");
+
+            UncommitedEvents.Add(new MoneyTransferred(Id, amount, iban));
+        }
+    }
+
+    internal class AccountOpened : Event
+    {
+        private Guid id;
+        private string owner;
+        private string iban;
+
+        public AccountOpened(Guid id, string owner, string iban)
+        {
+            this.id = id;
+            this.owner = owner;
+            this.iban = iban;
+        }
+    }
+
+    public class Event
+    {
+    }
+}
---
+++ EsAndCqrs/MoneyTransferred.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EsAndCqrs
+{
+    internal class MoneyTransferred : Event
+    {
+        public Guid Id { get; private set; }
+        public decimal Amount { get; private set; }
+        public string Iban { get; private set; }
+
+        public MoneyTransferred(Guid id, decimal amount, string iban)
+        {
+            Id = id;
+            Amount = amount;
+            Iban = iban;
+        }
+    }
+}
