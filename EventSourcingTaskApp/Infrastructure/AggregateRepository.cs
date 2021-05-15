8958dc755608f4e307c0a002bf98e335597511b4
Dong Zeng <dong@freshbytesoftware.com>
2021-05-15 10:56:20 -0500

    added Infrastructure.AggregateRepository

---
+++ EventSourcingTaskApp/Infrastructure/AggregateRepository.cs
@@ -0,0 +1,14 @@
+﻿using EventStore.ClientAPI;
+
+namespace EventSourcingTaskApp.Infrastructure
+{
+    public class AggregateRepository
+    {
+        private readonly IEventStoreConnection _eventStore;
+
+        public AggregateRepository(IEventStoreConnection eventStore)
+        {
+            _eventStore = eventStore;
+        }
+    }
+}
