4377a495b2d94fcde49facabac966601f85fa08a
Dong Zeng <dong@freshbytesoftware.com>
2021-05-15 11:15:11 -0500

    added LoadAsync to AggregateRepository

--- EventSourcingTaskApp/Infrastructure/AggregateRepository.cs
+++ EventSourcingTaskApp/Infrastructure/AggregateRepository.cs
@@ -39,6 +39,37 @@ namespace EventSourcingTaskApp.Infrastructure
             var result = await _eventStore.AppendToStreamAsync(streamName, ExpectedVersion.Any, events);
         }
 
+        public async Task<T> LoadAsync<T>(Guid aggregateId) where T : Aggregate, new()
+        {
+            if (aggregateId == Guid.Empty)
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(aggregateId));
+
+            var aggregate = new T();
+            var streamName = GetStreamName(aggregate, aggregateId);
+
+            var nextPageStart = 0L;
+
+            do
+            {
+                var page = await _eventStore.ReadStreamEventsForwardAsync(streamName, nextPageStart, 4096, false);
+
+                if (page.Events.Length > 0)
+                {
+                    aggregate.Load(
+                        page.Events.Last().Event.EventNumber,
+                        page.Events
+                            .Select(@event => JsonSerializer.Deserialize(Encoding.UTF8.GetString(@event.OriginalEvent.Data),
+                            Type.GetType(Encoding.UTF8.GetString(@event.OriginalEvent.Metadata)))
+                            ).ToArray());
+                }
+
+                nextPageStart = !page.IsEndOfStream ? page.NextEventNumber : -1;
+            }
+            while (nextPageStart != -1);
+
+            return aggregate;
+        }
+
         private string GetStreamName<T>(T type, Guid aggregateId) => $"{type.GetType().Name}-{aggregateId}";
     }
 }
