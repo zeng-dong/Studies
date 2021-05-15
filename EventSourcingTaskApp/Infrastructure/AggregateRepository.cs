6a9c267808a4364ca02d2218e340255e90a8cf8e
Dong Zeng <dong@freshbytesoftware.com>
2021-05-15 11:05:53 -0500

    added SaveAsync to AggregateRepository

--- EventSourcingTaskApp/Infrastructure/AggregateRepository.cs
+++ EventSourcingTaskApp/Infrastructure/AggregateRepository.cs
@@ -1,4 +1,11 @@
-﻿using EventStore.ClientAPI;
+﻿using EventSourcingTaskApp.Core.Framework;
+using EventStore.ClientAPI;
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace EventSourcingTaskApp.Infrastructure
 {
@@ -10,5 +17,28 @@ namespace EventSourcingTaskApp.Infrastructure
         {
             _eventStore = eventStore;
         }
+
+        public async Task SaveAsync<T>(T aggregate) where T : Aggregate, new()
+        {
+            var events = aggregate.GetChanges()
+                .Select(@event => new EventData(
+                    Guid.NewGuid(),
+                    @event.GetType().Name,
+                    true,
+                    Encoding.UTF8.GetBytes(JsonSerializer.Serialize(@event)),
+                    Encoding.UTF8.GetBytes(@event.GetType().FullName)
+                    )).ToArray();
+
+            if (!events.Any())
+            {
+                return;
+            }
+
+            var streamName = GetStreamName(aggregate, aggregate.Id);
+
+            var result = await _eventStore.AppendToStreamAsync(streamName, ExpectedVersion.Any, events);
+        }
+
+        private string GetStreamName<T>(T type, Guid aggregateId) => $"{type.GetType().Name}-{aggregateId}";
     }
 }
