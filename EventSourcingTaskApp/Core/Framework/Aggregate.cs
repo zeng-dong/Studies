040b9f34d0f93c7351f2a1d0c03c5f3997d912c2
Dong Zeng <dong@freshbytesoftware.com>
2021-05-14 20:42:49 -0500

    add Core.Framework.Aggregate

---
+++ EventSourcingTaskApp/Core/Framework/Aggregate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventSourcingTaskApp.Core.Framework
+{
+    public abstract class Aggregate
+    {
+        readonly IList<object> _changes = new List<object>();
+
+        public Guid Id { get; protected set; } = Guid.Empty;
+        public long Version { get; private set; }
+    }
+}
