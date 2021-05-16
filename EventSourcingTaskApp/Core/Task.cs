459d56796e39030eaaec1718ef0f215b73278d23
Dong Zeng <dong@freshbytesoftware.com>
2021-05-16 11:38:11 -0500

    added Task class derived from Aggregate

---
+++ EventSourcingTaskApp/Core/Task.cs
@@ -0,0 +1,18 @@
+﻿using EventSourcingTaskApp.Core.Framework;
+using System;
+
+namespace EventSourcingTaskApp.Core
+{
+    public class Task : Aggregate
+    {
+        public string Title { get; private set; }
+        public BoardSections Section { get; private set; }
+        public string AssignedTo { get; private set; }
+        public bool IsCompleted { get; private set; }
+
+        protected override void When(object @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
