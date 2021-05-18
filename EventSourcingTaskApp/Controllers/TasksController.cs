f5ee7b2ed111275f230ac3364920964076291799
Dong Zeng <dong@freshbytesoftware.com>
2021-05-18 16:58:00 -0500

    tadded Create and Assign endpoints

--- EventSourcingTaskApp/Controllers/TasksController.cs
+++ EventSourcingTaskApp/Controllers/TasksController.cs
@@ -1,5 +1,7 @@
 ﻿using EventSourcingTaskApp.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace EventSourcingTaskApp.Controllers
 {
@@ -13,6 +15,28 @@ namespace EventSourcingTaskApp.Controllers
         public TasksController(AggregateRepository aggregateRepository)
         {
             _aggregateRepository = aggregateRepository;
+        }
+
+        [HttpPost, Route("create")]
+        public async Task<IActionResult> Create(Guid id, [FromForm] string title)
+        {
+            var aggregate = await _aggregateRepository.LoadAsync<Core.Task>(id);
+            aggregate.Create(id, title, "Fresh Bite");
+
+            await _aggregateRepository.SaveAsync(aggregate);
+
+            return Ok();
+        }
+
+        [HttpPatch, Route("assign")]
+        public async Task<IActionResult> Assign(Guid id, [FromForm] string assignedTo)
+        {
+            var aggregate = await _aggregateRepository.LoadAsync<Core.Task>(id);
+            aggregate.Assign(assignedTo, "Fresh Bite");
+
+            await _aggregateRepository.SaveAsync(aggregate);
+
+            return Ok();
         }
     }
 }
