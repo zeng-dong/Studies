b82a0780cd340aed9b42c980a28db9f232dbd378
Dong Zeng <dong@freshbytesoftware.com>
2021-05-18 16:13:06 -0500

    added TasksController

---
+++ EventSourcingTaskApp/Controllers/TasksController.cs
@@ -0,0 +1,18 @@
+﻿using EventSourcingTaskApp.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventSourcingTaskApp.Controllers
+{
+    [Route("api/tasks/{id}")]
+    [ApiController]
+    [Consumes("application/x-www-form-urlencoded")]
+    public class TasksController : ControllerBase
+    {
+        private readonly AggregateRepository _aggregateRepository;
+
+        public TasksController(AggregateRepository aggregateRepository)
+        {
+            _aggregateRepository = aggregateRepository;
+        }
+    }
+}
