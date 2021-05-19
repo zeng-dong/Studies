188cf1cd5d2751dd051fcf23e4943922ebc03817
Dong Zeng <dong@freshbytesoftware.com>
2021-05-19 14:06:05 -0500

    added DomainModel that stores events

--- EventStoreConsoleApp/DomainModel.cs
+++ EventStoreConsoleApp/DomainModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EventStoreConsoleApp
 {
@@ -6,5 +7,40 @@ namespace EventStoreConsoleApp
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public decimal CurrentBalance { get; set; }
+        public List<Transaction> Transactions => new List<Transaction>();
+
+        public void Apply(AccountCreated @event)
+        {
+            Id = @event.Id;
+            Name = @event.Name;
+            CurrentBalance = 0;
+        }
+
+        public void Apply(FundsDespoited @event)
+        {
+            var newTransaction = new Transaction { Id = @event.Id, Amount = @event.Amount };
+            Transactions.Add(newTransaction);
+            CurrentBalance = CurrentBalance + @event.Amount;
+        }
+
+        public void Apply(FundsWithdrawed @event)
+        {
+            var newTransaction = new Transaction { Id = @event.Id, Amount = @event.Amount };
+            Transactions.Add(newTransaction);
+            CurrentBalance = CurrentBalance - @event.Amount;
+        }
+    }
+
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public decimal Amount { get; set; }
+
+        //public Transaction(Guid id, decimal amount)
+        //{
+        //    Id = id;
+        //    Amount = amount;
+        //}
     }
 }
