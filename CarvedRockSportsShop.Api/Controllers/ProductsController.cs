29fb42dfdb4a547238ee869b026591530b42a829
Dong Zeng <dong@freshbytesoftware.com>
2021-05-20 08:46:17 -0500

    added CarvedRockSportsShop to code along Docker

---
+++ CarvedRockSportsShop.Api/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using CarvedRock.Api.ApiModels;
+using CarvedRock.Api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarvedRock.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductLogic _productLogic;
+
+        public ProductsController(IProductLogic productLogic)
+        {
+            _productLogic = productLogic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts(string category = "all")
+        {
+            return _productLogic.GetProductsForCategory(category);
+        }
+    }
+}
