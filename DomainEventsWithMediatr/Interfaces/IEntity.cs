baeb518734e187add5b27fdaf517c8b6f8d15339
Dong Zeng <dong@freshbytesoftware.com>
2021-05-24 12:33:46 -0500

    started DomainEventsWtihMediatr

---
+++ DomainEventsWithMediatr/Interfaces/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace DomainEventsWithMediatr.Interfaces
+{
+    public interface IEntity
+    {
+        Guid Id { get; }
+        List<INotification> Events { get; }
+    }
+}
---
+++ DomainEventsWithMediatr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DomainEventsWithMediatr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
