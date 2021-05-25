0620860751d2aea47f984d496a20cce0fe3a33b6
Dong Zeng <dong@freshbytesoftware.com>
2021-05-25 16:54:13 -0500

    added on concrete Repository

---
+++ DomainEventsWithMediatr/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using DomainEventsWithMediatr.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DomainEventsWithMediatr.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : IEntity
+    {
+        private readonly Dictionary<Guid, TEntity> _entities = new Dictionary<Guid, TEntity>();
+        private readonly IMediator _mediator;
+
+        public Repository(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        public List<TEntity> GetAll()
+        {
+            return _entities.Values.ToList();
+        }
+
+        public TEntity GetById(Guid id)
+        {
+            return _entities[id];
+        }
+
+        public async Task Save(TEntity entity)
+        {
+            _entities[entity.Id] = entity;
+            //ConsoleWriter.FromRepositories("[DATABASE] Saved entity {0}", entity.Id.ToString());
+
+            var eventsCopy = entity.Events.ToArray();
+            entity.Events.Clear();
+            foreach (var domainEvent in eventsCopy)
+            {
+                await _mediator.Publish(domainEvent).ConfigureAwait(false);
+            }
+
+        }
+    }
+}
