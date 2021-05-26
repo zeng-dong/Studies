feda076dd2017fcb2ea9c9595b48b0cbd15e7e5d
Dong Zeng <dong@freshbytesoftware.com>
2021-05-26 10:07:46 -0500

    setup EFCore5 GettingStarted project

--- TablePerHierarchy/ConcreteBaseClass/ImplictDerivedTypesDbContext.cs
+++ TablePerHierarchy/ConcreteBaseClass/ImplictDerivedTypesDbContext.cs
@@ -11,6 +11,14 @@ namespace TablePerHierarchy.ConcreteBaseClass
             modelBuilder.Entity<TvContract>();
             modelBuilder.Entity<BroadBandContract>();
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=TPH_Implicit");
+
+            // base.OnConfiguring(optionsBuilder);          /// the base method won't do anything more for you
+        }
     }
 
     // so we can do: var tvContracts = context.Contracts.OfType<TvContract>().ToList();
