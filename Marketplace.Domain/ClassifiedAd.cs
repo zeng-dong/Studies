7a4a9fb45ccfef1fc3be24ba6a9ef9c885f672ef
Dong Zeng <dong@freshbytesoftware.com>
2021-06-02 13:48:43 -0500

    added behavior to ClassifiedAd

--- Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -9,5 +9,17 @@ namespace Marketplace.Domain
         private string _title;
         private string _text;
         private decimal _price;
+
+        public ClassifiedAd(Guid id)
+        {
+            if (id == default)
+                throw new ArgumentException("Identity must be specified", nameof(id));
+
+            Id = id;
+        }
+
+        public void SetTitle(string title) => _title = title;
+        public void UpdateText(string text) => _text = text;
+        public void UpdatePrice(decimal price) => _price = price;
     }
 }
