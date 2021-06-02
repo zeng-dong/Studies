8d79c3bee9315a2ec79b6e4cf879047722a556e4
Dong Zeng <dong@freshbytesoftware.com>
2021-06-02 13:46:30 -0500

    added Marketplace first entity

---
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAd
+    {
+        public Guid Id { get; private set; }
+        private Guid _ownerId;
+        private string _title;
+        private string _text;
+        private decimal _price;
+    }
+}
