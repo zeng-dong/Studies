4e90f1e2aa121b9ceb4eb934551e227250ca1b17
Dong Zeng <dong@freshbytesoftware.com>
2021-06-04 18:25:01 -0500

    Money implements IEquatable and unit test passes

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -1,8 +1,31 @@
-﻿namespace Marketplace.Domain
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Marketplace.Domain
 {
-    public class Money
+    public class Money : IEquatable<Money>
     {
         public decimal Amount { get; }
         public Money(decimal amount) => Amount = amount;
+
+        public bool Equals([AllowNull] Money other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Amount.Equals(other.Amount);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Money)obj);
+        }
+
+        public override int GetHashCode() => Amount.GetHashCode();
+
+        public static bool operator ==(Money left, Money right) => Equals(left, right);
+        public static bool operator !=(Money left, Money right) => !Equals(left, right);
     }
 }
