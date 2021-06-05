48333da0d6727347fe71180a0117aeefa34fd4b1
Dong Zeng <dong@freshbytesoftware.com>
2021-06-04 19:25:48 -0500

    added Email vo

---
+++ Marketplace.Domain/UsingFunctionalExtensions/Email.cs
@@ -0,0 +1,37 @@
+﻿using CSharpFunctionalExtensions;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace Marketplace.Domain.UsingFunctionalExtensions
+{
+    public class Email : ValueObject
+    {
+        public string Value { get; }
+
+        private Email(string value)
+        {
+            Value = value;
+        }
+        public static Result<Email, Error> Create(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return Errors.General.ValueIsRequired();
+
+            string email = input.Trim();
+
+            if (email.Length > 150)
+                return Errors.General.InvalidLength();
+
+            if (Regex.IsMatch(email, @"^(.+)@(.+)$") == false)
+                return Errors.General.ValueIsInvalid();
+
+            return new Email(email);
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
