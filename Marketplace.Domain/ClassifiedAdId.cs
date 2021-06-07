2d21ef18cf7f2b7e738c130191a9d15379ea16fa
Dong Zeng <dong@freshbytesoftware.com>
2021-06-06 21:44:34 -0500

    update ClassifiedAdId with Value<>

--- Marketplace.Domain/ClassifiedAdId.cs
+++ Marketplace.Domain/ClassifiedAdId.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using Marketplace.Framework;
+using System;
 
 namespace Marketplace.Domain
 {
-    public class ClassifiedAdId
+    public class ClassifiedAdId : Value<ClassifiedAdId>
     {
         private readonly Guid _value;
-        public ClassifiedAdId(Guid value)
-        {
-            if (value == default)
-                throw new ArgumentNullException(nameof(value), "Classified Ad id cannot be empty");
+        //public ClassifiedAdId(Guid value)
+        //{
+        //    if (value == default)
+        //        throw new ArgumentNullException(nameof(value), "Classified Ad id cannot be empty");
+        //
+        //    _value = value;
+        //}
 
-            _value = value;
-        }
+        public ClassifiedAdId(Guid value) => _value = value;
     }
 }
