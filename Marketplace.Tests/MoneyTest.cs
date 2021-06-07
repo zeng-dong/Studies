67b31f27a9d010fb4eef5ef55ec6610333a284ad
Dong Zeng <dong@freshbytesoftware.com>
2021-06-06 20:29:19 -0500

    new unit test

--- Marketplace.Tests/MoneyTest.cs
+++ Marketplace.Tests/MoneyTest.cs
@@ -12,5 +12,15 @@ namespace Marketplace.Tests
             var secondAmount = new Money(5);
             Assert.Equal(firstAmount, secondAmount);
         }
+
+        [Fact]
+        public void Sum_of_money_gives_full_amount()
+        {
+            var coin1 = new Money(1);
+            var coin2 = new Money(2);
+            var coin3 = new Money(2);
+            var banknote = new Money(5);
+            Assert.Equal(banknote, coin1 + coin2 + coin3);
+        }
     }
 }
