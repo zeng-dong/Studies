65aeb09e2f3552331ea4132fa64f11ac4ae38df2
Dong Zeng <dong@freshbytesoftware.com>
2021-06-08 17:51:05 -0500

    added value object ClassifiedAdTitle

---
+++ Marketplace.Domain/ClassifiedAdTitle.cs
@@ -0,0 +1,19 @@
+﻿using Marketplace.Framework;
+using System;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAdTitle : Value<ClassifiedAdTitle>
+    {
+        public static ClassifiedAdTitle FromString(string title) => new ClassifiedAdTitle(title);
+        private readonly string _value;
+        private ClassifiedAdTitle(string value)
+        {
+            if (value.Length > 100)
+                throw new ArgumentOutOfRangeException("Title cannot be longer that 100 characters", nameof(value));
+
+            _value = value;
+        }
+
+    }
+}
