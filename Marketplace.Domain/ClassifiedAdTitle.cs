b7829eee6c35f79e61c8012805355f1a89fd78aa
Dong Zeng <dong@freshbytesoftware.com>
2021-06-08 17:59:55 -0500

    added factory to ClassifiedAdTitle

--- Marketplace.Domain/ClassifiedAdTitle.cs
+++ Marketplace.Domain/ClassifiedAdTitle.cs
@@ -1,10 +1,21 @@
 ﻿using Marketplace.Framework;
 using System;
+using System.Text.RegularExpressions;
 
 namespace Marketplace.Domain
 {
     public class ClassifiedAdTitle : Value<ClassifiedAdTitle>
     {
+        public static ClassifiedAdTitle FromHtml(string htmlTitle)
+        {
+            var supportedTagsReplaced = htmlTitle
+            .Replace("<i>", "*")
+            .Replace("</i>", "*")
+            .Replace("<b>", "**")
+            .Replace("</b>", "**");
+            return new ClassifiedAdTitle(Regex.Replace(supportedTagsReplaced, "<.*?>", string.Empty));
+        }
+
         public static ClassifiedAdTitle FromString(string title) => new ClassifiedAdTitle(title);
         private readonly string _value;
         private ClassifiedAdTitle(string value)
