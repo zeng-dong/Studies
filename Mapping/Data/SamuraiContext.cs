f9b81bffa7d14fedece1f3fbeb6f217285b47108
Dong Zeng <dong@freshbytesoftware.com>
2021-06-11 09:49:33 -0500

    migration InitialCreate

--- Mapping/Data/SamuraiContext.cs
+++ Mapping/Data/SamuraiContext.cs
@@ -1,18 +1,33 @@
 ﻿using Mapping.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Mapping.Data
 {
     public class SamuraiContext : DbContext
     {
+        //public static readonly LoggerFactory MyConsoleLoggerFactory
+        //   = new LoggerFactory(new[] {
+        //      new ConsoleLoggerProvider((category, level)
+        //        => category == DbLoggerCategory.Database.Command.Name
+        //       && level == LogLevel.Information, true) });
+
+        public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(
+            builder =>
+            {
+                builder.AddConsole();
+            });
+
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
-                 "Server = (localdb)\\mssqllocaldb; Database = Samurai_Mapping; Trusted_Connection = True; ");
+            optionsBuilder
+                .UseLoggerFactory(MyLoggerFactory)
+                .EnableSensitiveDataLogging(true)
+                .UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = Samurai_Mapping; Trusted_Connection = True; ");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ Mapping/Migrations/20210611144524_InitialCreate.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mapping.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Battles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Battles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Samurais",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Samurais", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(nullable: true),
+                    SamuraiId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Quotes_Samurais_SamuraiId",
+                        column: x => x.SamuraiId,
+                        principalTable: "Samurais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SamuraiBattle",
+                columns: table => new
+                {
+                    SamuraiId = table.Column<int>(nullable: false),
+                    BattleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SamuraiBattle", x => new { x.SamuraiId, x.BattleId });
+                    table.ForeignKey(
+                        name: "FK_SamuraiBattle_Battles_BattleId",
+                        column: x => x.BattleId,
+                        principalTable: "Battles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SamuraiBattle_Samurais_SamuraiId",
+                        column: x => x.SamuraiId,
+                        principalTable: "Samurais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SecretIdentity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RealName = table.Column<string>(nullable: true),
+                    SamuraiId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecretIdentity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SecretIdentity_Samurais_SamuraiId",
+                        column: x => x.SamuraiId,
+                        principalTable: "Samurais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quotes_SamuraiId",
+                table: "Quotes",
+                column: "SamuraiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SamuraiBattle_BattleId",
+                table: "SamuraiBattle",
+                column: "BattleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecretIdentity_SamuraiId",
+                table: "SecretIdentity",
+                column: "SamuraiId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotes");
+
+            migrationBuilder.DropTable(
+                name: "SamuraiBattle");
+
+            migrationBuilder.DropTable(
+                name: "SecretIdentity");
+
+            migrationBuilder.DropTable(
+                name: "Battles");
+
+            migrationBuilder.DropTable(
+                name: "Samurais");
+        }
+    }
+}
