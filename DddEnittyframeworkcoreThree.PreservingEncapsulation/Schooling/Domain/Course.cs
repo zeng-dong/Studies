d0248ae2be00342c740e8436ff7beb1aaad9e739
Dong Zeng <dong@freshbytesoftware.com>
2021-06-12 10:03:06 -0500

    added Course and Student in Schooling.Domain

---
+++ DddEnittyframeworkcoreThree.PreservingEncapsulation/Schooling/Domain/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace DddEnittyframeworkcoreThree.PreservingEncapsulation.Schooling.Domain
+{
+    public class Course
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Credits { get; set; }
+    }
+}
---
+++ DddEnittyframeworkcoreThree.PreservingEncapsulation/Schooling/Domain/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace DddEnittyframeworkcoreThree.PreservingEncapsulation.Schooling.Domain
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long FavoriteCourseId { get; set; }
+    }
+}
