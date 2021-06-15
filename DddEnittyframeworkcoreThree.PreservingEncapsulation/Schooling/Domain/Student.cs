c1c1b72c8e2341ea0a4674654f4a8d207343a544
Dong Zeng <dong@freshbytesoftware.com>
2021-06-14 21:21:29 -0500

    getting rid of public setters of Student

--- DddEnittyframeworkcoreThree.PreservingEncapsulation/Schooling/Domain/Student.cs
+++ DddEnittyframeworkcoreThree.PreservingEncapsulation/Schooling/Domain/Student.cs
@@ -2,9 +2,23 @@
 {
     public class Student
     {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public long FavoriteCourseId { get; set; }
+        public long Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public long FavoriteCourseId { get; private set; }
+
+        public Student(string name, string email, long favoriteCouseId)
+        {
+            Name = name;
+            Email = email;
+            FavoriteCourseId = favoriteCouseId;
+        }
     }
+
+    // public setters: not easy to maintain invariant
+    // efc binds to backing fields by default, that is why we can use private setters, or even no setters
+
+    // efc 3.x can instantiate object with custom ctor, so no need for parameter-less ctor (public or private)?
+    //    but these parameter names must match the Property Names
+    //    if public Student(string name2, string email, long favoriteCouseId), for example, we get InvalidOperationException
 }
