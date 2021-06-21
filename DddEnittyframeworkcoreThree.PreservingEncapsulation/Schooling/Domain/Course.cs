60bca8afa0e839c353a331a2ffe9d67d58b35167
Dong Zeng <dong@freshbytesoftware.com>
2021-06-21 08:45:46 -0500

    using Enumeration pattern in Course

--- DddEnittyframeworkcoreThree.PreservingEncapsulation/Schooling/Domain/Course.cs
+++ DddEnittyframeworkcoreThree.PreservingEncapsulation/Schooling/Domain/Course.cs
@@ -4,7 +4,24 @@ namespace DddEnittyframeworkcoreThree.PreservingEncapsulation.Schooling.Domain
 {
     public class Course : Entity
     {
+        public static readonly Course Calculus = new Course(1, "Calculus");
+        public static readonly Course Chemistry = new Course(2, "Chemistry");
+
+        public static readonly Course[] AllCourses = { Calculus, Chemistry };
+
+
+
         public string Name { get; }
         //public string Credits { get; set; }
+
+        protected Course()
+        {
+        }
+
+        private Course(long id, string name)
+            : base(id)
+        {
+            Name = name;
+        }
     }
 }
