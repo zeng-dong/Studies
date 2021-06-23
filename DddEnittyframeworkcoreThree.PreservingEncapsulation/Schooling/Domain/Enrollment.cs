94bf3e1924d87ca070ff137a0b4be36127408002
Dong Zeng <dong@freshbytesoftware.com>
2021-06-22 21:10:28 -0500

    added Enrollment.cs and Grade.cs

---
+++ DddEnittyframeworkcoreThree.PreservingEncapsulation/Schooling/Domain/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using DddEnittyframeworkcoreThree.PreservingEncapsulation.Core;
+
+namespace DddEnittyframeworkcoreThree.PreservingEncapsulation.Schooling.Domain
+{
+    public class Enrollment : Entity
+    {
+        public Grade Grade { get; }
+        public virtual Course Course { get; }
+        public virtual Student Student { get; }
+
+        protected Enrollment()
+        {
+        }
+
+        public Enrollment(Course course, Student student, Grade grade)
+            : this()
+        {
+            Course = course;
+            Student = student;
+            Grade = grade;
+        }
+    }
+}
---
+++ DddEnittyframeworkcoreThree.PreservingEncapsulation/Schooling/Domain/Grade.cs
@@ -0,0 +1,11 @@
+﻿namespace DddEnittyframeworkcoreThree.PreservingEncapsulation.Schooling.Domain
+{
+    public enum Grade
+    {
+        A = 0,
+        B = 1,
+        C = 2,
+        D = 3,
+        F = 4
+    }
+}
