4560efd4b9d682e64cfba62aede85da47f955fc5
Dong Zeng <dong@freshbytesoftware.com>
2021-07-02 08:58:19 -0500

    added Suffix enumeration

---
+++ DddEnittyframeworkcoreThree.PreservingEncapsulation/Schooling/Domain/Suffix.cs
@@ -0,0 +1,30 @@
+﻿using CSharpFunctionalExtensions;
+using System.Linq;
+
+namespace DddEnittyframeworkcoreThree.PreservingEncapsulation.Schooling.Domain
+{
+    public class Suffix : Entity
+    {
+        public static readonly Suffix Jr = new Suffix(1, "Jr");
+        public static readonly Suffix Sr = new Suffix(2, "Sr");
+
+        public static readonly Suffix[] AllSuffixes = { Jr, Sr };
+
+        public string Name { get; }
+
+        protected Suffix()
+        {
+        }
+
+        private Suffix(long id, string name)
+            : base(id)
+        {
+            Name = name;
+        }
+
+        public static Suffix FromId(long id)
+        {
+            return AllSuffixes.SingleOrDefault(x => x.Id == id);
+        }
+    }
+}
