32cb4ce6eb1712aad85552e2711dfda626229e32
Dong Zeng <dong@freshbytesoftware.com>
2021-07-07 20:59:45 -0500

    StudentEmailChangedEvent

--- DddEnittyframeworkcoreThree.PreservingEncapsulation/Core/IDomainEvent.cs
+++ DddEnittyframeworkcoreThree.PreservingEncapsulation/Core/IDomainEvent.cs
@@ -1,6 +1,20 @@
-﻿namespace DddEnittyframeworkcoreThree.PreservingEncapsulation.Core
+﻿using DddEnittyframeworkcoreThree.PreservingEncapsulation.Schooling.Domain;
+
+namespace DddEnittyframeworkcoreThree.PreservingEncapsulation.Core
 {
     public interface IDomainEvent
     {
+    }
+
+    public class StudentEmailChangedEvent : IDomainEvent
+    {
+        public long StudentId { get; }
+        public Email NewEmail { get; }
+
+        public StudentEmailChangedEvent(long studentId, Email newEmail)
+        {
+            StudentId = studentId;
+            NewEmail = newEmail;
+        }
     }
 }
