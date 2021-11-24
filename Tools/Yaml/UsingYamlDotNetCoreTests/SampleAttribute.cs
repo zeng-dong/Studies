8d748adebea8660560372692f5219d9003374fa9
Dong Zeng <dong@freshbyte.com>
2021-11-24 09:42:22 -0600

    loading a stream: no difference than standard

---
+++ Tools/Yaml/UsingYamlDotNetCoreTests/SampleAttribute.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+
+namespace UsingYamlDotNetCoreTests
+{
+    public class SampleAttribute : FactAttribute
+    {
+        public string Description { get; set; }
+    }
+}
---
+++ Tools/Yaml/UsingYamlDotNetCoreTests/TestOutputHelperExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Xunit.Abstractions;
+
+namespace UsingYamlDotNetCoreTests
+{
+    public static class TestOutputHelperExtensions
+    {
+        public static void WriteLine(this ITestOutputHelper output)
+        {
+            output.WriteLine(string.Empty);
+        }
+    }
+}
