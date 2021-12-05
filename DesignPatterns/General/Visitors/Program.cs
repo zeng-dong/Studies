0e69901a2a0fd2805bfc270d3b6524675777777c
Dong Zeng <dong@freshbyte.com>
2021-12-04 19:05:25 -0600

    added the two interfaces

--- DesignPatterns/General/Visitors/Program.cs
+++ DesignPatterns/General/Visitors/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Visitors
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Object> items = new List<object>
+            {
+                new Book(12345, 11.99),
+                new Book(78910, 22.79),
+                new Vinyl(11198, 17.99),
+                new Book(66634, 9.99)
+            };
         }
     }
 }
---
+++ DesignPatterns/General/Visitors/Visitor.cs
@@ -0,0 +1,16 @@
+﻿namespace Visitors
+{
+    public interface IVisitor
+    {
+        void VisitBook(Book book);
+
+        void VisitViny(Vinyl vinyl);
+
+        void Print();
+    }
+
+    public interface IVisitableElement
+    {
+        void Accetp(IVisitor visitor);
+    }
+}
