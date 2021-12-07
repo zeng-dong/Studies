cbe5df42f0224b970e6ce1fb6dc6ed12d413c452
Dong Zeng <dong@freshbyte.com>
2021-12-07 13:38:03 -0600

    try to test the rules with rule evaluator

--- DesignPatterns/General/Rules/Program.cs
+++ DesignPatterns/General/Rules/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Rules.Rules;
+using System;
 
 namespace Rules
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            TestIndex.testIndex();
+        }
+
+        private static void ruleExample()
+        {
+            var ctx = new Context() { Date = new DateTime(2014, 7, 28) };
+
+            var rules = new IRule[] {
+                new VerbTenses(),
+                new FactCheck()
+            };
+            new RuleEvaulator(rules).Execute(ctx);
         }
     }
 }
