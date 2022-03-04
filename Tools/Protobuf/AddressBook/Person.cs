8eb7ecbedcef27ab02baf73f53c01113b81242ad
Dong Zeng <zeng-dong@hotmail.com>
2022-03-04 13:19:10 -0600

    trying protobuf.net

---
+++ Tools/Protobuf/AddressBook/Person.cs
@@ -0,0 +1,16 @@
+﻿using ProtoBuf;
+
+namespace AddressBook;
+
+[ProtoContract]
+class Person
+{
+    [ProtoMember(1)]
+    public string FirstName { get; set; }
+
+    [ProtoMember(2)]
+    public string LastName { get; set; }
+
+    [ProtoMember(3)]
+    public List<string> Emails { get; set; }
+}
---
+++ Tools/Protobuf/AddressBook/Program.cs
@@ -0,0 +1,37 @@
+﻿using ProtoBuf;
+
+namespace AddressBook;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var person = new Person()
+        {
+            FirstName = "Wade",
+            LastName = "Smith",
+            Emails = new List<string>
+            {
+                "wade.smith@gmail.com",
+                "wsmith@company.com"
+            }
+        };
+
+        //using (var memoryStream = new MemoryStream())
+        //{
+        //    Serializer.Serialize(memoryStream, person);
+        //    var byteArray = memoryStream.ToArray();
+        //}
+
+        using (var fileStream = File.Create("person.buf"))
+        {
+            Serializer.Serialize(fileStream, person);
+        }
+
+        using (var fileStream = File.OpenRead("person.buf"))
+        {
+            var myPerson = Serializer.Deserialize<Person>(fileStream);
+            Console.WriteLine(myPerson.FirstName);
+        }
+    }
+}
