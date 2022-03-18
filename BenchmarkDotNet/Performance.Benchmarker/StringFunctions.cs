511d753ec16d06785c0da26998b8310cc7550869
Dong Zeng <zeng-dong@hotmail.com>
2022-03-18 09:56:45 -0500

    benchmarking two methods

---
+++ BenchmarkDotNet/Performance.Benchmarker/StringFunctions.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using Performance.Code;
+
+namespace Performance.Benchmarker;
+
+public class StringFunctions
+{
+    [Benchmark()]
+    public void BuildStringBadly()
+    {
+        var x = new StringWorker().BuildBadly("test");
+    }
+
+    [Benchmark()]
+    public void BuildStringBetter()
+    {
+        var x = new StringWorker().BuildBetter("test");
+    }
+}
