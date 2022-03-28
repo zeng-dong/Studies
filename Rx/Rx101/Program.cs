c9a1fd9979bc0deabbe80310223ae181766ae20e
Dong Zeng <zeng-dong@hotmail.com>
2022-03-28 08:58:34 -0500

    adding rx

---
+++ Rx/Rx101/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Rx101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var query = from number in Enumerable.Range(1, 10) select number;
+            var oq = query.ToObservable();
+
+            oq.Subscribe(Console.WriteLine, () => { Console.WriteLine("I am done."); });
+        }
+    }
+}
